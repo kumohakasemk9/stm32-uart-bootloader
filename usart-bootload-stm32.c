eb04b2b1587bdfa70f9e7712fb314841e4629f55
kumohakasemk9 <87934760+kumohakasemk9@users.noreply.github.com>
2022-06-22 09:40:39 +0900

    Update usart-bootload-stm32.c

--- usart-bootload-stm32.c
+++ usart-bootload-stm32.c
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2022 kumohakase (license: https://creativecommons.org/licenses/by/2.5/)
+Program loader for stm32 uart bootloader protocol (Load to RAM)
+Please consider supporting me using kofi.com https://ko-fi.com/kumohakase
+*/
+
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
